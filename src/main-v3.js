9a3ca7b79450e4e423eb214831530c9c73cc75d7
Xavier Decuyper <xavier.decuyper@gmail.com>
2018-02-03 09:47:14 +0100

    Updating creation of Genesis block

--- src/main-v3.js
+++ src/main-v3.js
@@ -41,7 +41,7 @@ class Blockchain{
     }
 
     createGenesisBlock() {
-        return new Block("01/01/2017", "Genesis block", "0");
+        return new Block(Date.parse("2017-01-01"), [], "0");
     }
 
     getLatestBlock() {
