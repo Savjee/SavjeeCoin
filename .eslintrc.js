b2dcfa0deac9325453ab1c6589f9da30b3a7db38
Tobias <tobigames200@gmail.com>
2018-12-08 17:59:51 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,6 @@ module.exports = {
     "rules": {
         "semi": [2, "always"],
         "space-before-function-paren": ["error", "never"],
-        "skipBlankLines": true,
+        "no-trailing-spaces": ["error", { "skipBlankLines": true }],
     }
 };
