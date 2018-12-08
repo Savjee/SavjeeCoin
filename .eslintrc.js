b85361c0da421ff7f665d987230f2c31b404475b
Tobias <tobigames200@gmail.com>
2018-12-08 18:02:51 +0100

    Finally fixed this eslint stuff!

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
         "semi": [2, "always"],
         "space-before-function-paren": ["error", "never"],
         "no-trailing-spaces": ["error", { "skipBlankLines": true }],
+        "no-multiple-empty-lines": ["error", { "max": 4, "maxEOF": 2 }],
     }
 };
