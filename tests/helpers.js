83af4bf1c3dbf6d8e8b3a729563187910488fd78
Xavier Decuyper <hi@savjee.be>
2019-05-01 21:15:03 +0200

    Helper: allow to set custom amount

--- tests/helpers.js
+++ tests/helpers.js
@@ -3,8 +3,8 @@ const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 const signingKey = ec.keyFromPrivate('3d6f54430830d388052865b95c10b4aeb1bbe33c01334cf2cfa8b520062a0ce3');
 
-function createSignedTx(){
-	const txObject = new Transaction(signingKey.getPublic('hex'), 'wallet2', 10);
+function createSignedTx(amount = 10){
+	const txObject = new Transaction(signingKey.getPublic('hex'), 'wallet2', amount);
     txObject.timestamp = 1;
     txObject.signTransaction(signingKey);
 
