9f08dcaa36e70cfea34d8ab718d09f2e7a14da70
blackswan-03 <69258728+blackswan-03@users.noreply.github.com>
2022-07-22 15:44:51 -0400

    Modify keygenerator.js
    Re-formats keygenerator.js to "Prettier" standards. Also implements "strict mode."

--- src/keygenerator.js
+++ src/keygenerator.js
@@ -1,3 +1,4 @@
+'use strict';
 const EC = require('elliptic').ec;
 
 // You can use any elliptic curve you want
@@ -10,7 +11,13 @@ const privateKey = key.getPrivate('hex');
 
 // Print the keys to the console
 console.log();
-console.log('Your public key (also your wallet address, freely shareable)\n', publicKey);
+console.log(
+  'Your public key (also your wallet address, freely shareable)\n',
+  publicKey
+);
 
 console.log();
-console.log('Your private key (keep this secret! To sign transactions)\n', privateKey);
+console.log(
+  'Your private key (keep this secret! To sign transactions)\n',
+  privateKey
+);
